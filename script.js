1e7c1711ff0c846fb0eaca8bf00313a9a9377aa4
Andrew <125561357+PANATARA@users.noreply.github.com>
2023-06-06 00:21:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,22 @@ function changeText(Number){
 
 
 document.addEventListener("keydown", function(event) {
+    var letterPattern = /^[a-zA-Zа-яА-Я-" "-,]$/;
+    if (letterPattern.test(event.key)) {
     textContent = document.getElementById('Text').textContent;
     var letter = event.key;
     var before = textContent.substring(0, ind);
-    var after = textContent.substring(ind + 1);
+    var afterSymbol = textContent.substring(ind + 1, ind + 2); 
+    console.log(afterSymbol);
+    var after = textContent.substring(ind + 2);
     if (letter == textContent[ind]){
-      text.innerHTML = "<span style='color: green; font-weight: bold;'>" + before + "</span>" + "<span style='color: green; font-weight: bold;'>" + letter + "</span>" +  after;
+      text.innerHTML = "<span style='color: green;'>" + before + "</span>" + 
+      "<span style='color: green;'>" + letter + "</span>" + 
+      "<span style='text-decoration: underline; text-decoration-skip-ink: none; text-decoration-color: black;'>" + 
+      afterSymbol + "</span>" + after;
       ind++
     } else {
-        text.innerHTML = "<span style='color: green; font-weight: bold;'>" + before + "</span>" + "<span style='color: red; font-weight: bold;'>" + textContent[ind] + "</span>" +  after;
+        text.innerHTML = "<span style='color: green;'>" + before + "</span>" + "<span style='color: red;'>" + textContent[ind] + "</span>" + afterSymbol +  after;
     }
+}
 }); 
