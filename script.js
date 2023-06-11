a1f1e183084d255a5aae1c023eb48f2329fdd02a
Andrew <125561357+PANATARA@users.noreply.github.com>
2023-06-11 15:19:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var textContent = text.textContent;
 var remainAndSetting = document.getElementById('remainAndSetting');
 var ind = 0;
 const items = document.querySelectorAll('.section_item');
+let isFunctionExecuted = false;
 
 function randomNum(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,7 +14,7 @@ function changeText(Number){
   document.addEventListener('keydown', function(event) {
     if (event.keyCode === 32) {
       event.preventDefault();
-    }
+}
   });
     var textNow = "text" + randomNum(1,2);
     fetch('Texts.json')
@@ -32,6 +33,10 @@ document.addEventListener("keydown", function(event) {
     var before = textContent.substring(0, ind);
     var afterSymbol = textContent.substring(ind + 1, ind + 2); 
     var after = textContent.substring(ind + 2);
+    Hints(60);
+    if (ind == textContent.length - 1){
+      console.log(1234);
+    }
     if (letter == textContent[ind]){
       text.innerHTML = "<span style='color: green;'>" + before + "</span>" + 
       "<span style='color: green;'>" + letter + "</span>" + 
@@ -69,13 +74,13 @@ function theme(){
 }
 
 function cancelSpace(event) {
-    if (event.keyCode === 32) {
-      event.preventDefault();
-      return false;
-    }
+  if (event.keyCode === 32) {
+    event.preventDefault();
+    return false;
+  }
 }
 
-const tds = document.getElementsByTagName('td');
+var tds = document.querySelectorAll('.setting td');
 
 for (let i = 0; i < tds.length; i++) {
   tds[i].addEventListener('click', function() {
@@ -90,4 +95,17 @@ for (let i = 0; i < tds.length; i++) {
       tds[i + 1].style.color = "rgba(255, 255, 255, 1)";
     }
   });
+}
+
+function Hints(selectedTime){
+  if (!isFunctionExecuted) {
+    isFunctionExecuted = true;
+    var time = selectedTime;
+    setInterval(function() {
+      document.getElementById('remainTimeNum').innerHTML = time - 1;
+      time = time- 1;
+      document.getElementById('remainSymboolNum').innerHTML = textContent.length - ind;
+      document.getElementById('remainSymboolSpeedNum').innerHTML = Math.round((ind / (60 - time))*60);
+    }, 1000);
+  }
 }
